688f18eacb0a32ce75d85804a02a72b3a801a4ff
Syahrial Rafky <syahrafky08@gmail.com>
2023-12-10 20:04:00 +0700

    apa coba bingung nulis pesan

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/my-react-app/',
-  build: {
-    outDir: 'docs',
-  },
+  base: 'skylumi.github.io',
   plugins: [react()],
 })
