1787f14a003162309149ba011ef43677ae9709be
Syahrial Rafky <syahrafky08@gmail.com>
2023-12-12 22:11:10 +0700

    perubahan kecil

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import skylumiLogo from './assets/logo.png'
 import './App.css'
 
 function App() {
@@ -8,19 +7,22 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      <div className="container">
+        <section className="satu">
+        <a href="https://github.com/SkyLumi" target="_blank">
+          <img src= {skylumiLogo} className="logo"/>
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          <h1>Introducing Syahrial Rafky</h1>
+          <button>Start</button>
+        </section>
+
+        <section className="dua">
+        <p>
+          Hi! I'am Syahrial Rafky or you can just call me Rafky to avoid tongue cramps, an undergraduate Information Technology student that can do nothing!
+        </p>
+        <h2>My hard skill</h2>
+        </section>
       </div>
-      <h1>Introducing Syahrial Rafky</h1>
-      <p>
-        Hi! I'am Syahrial Rafky or you can just call me Rafky to avoid tongue cramps, an undergraduate Information Technology student that can do nothing!
-      </p>
-      <h2>My hard skill</h2>
     </>
   )
 }
